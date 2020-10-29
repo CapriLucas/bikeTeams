2d22494e176192d8d1d78e3ab0a3e2df1d4d0e66
LucasCapri <capriatalucas@gmail.com>
2020-10-29 20:37:54 -0300

    Added edit and delete routes

--- src/routes/tour.js
+++ src/routes/tour.js
@@ -2,6 +2,7 @@ const express = require("express");
 const pool = require("../database.js");
 const router = express.Router();
 const { hasNotTeam, hasTeam } = require("../lib/team.js");
+const moment = require("moment");
 
 router.get("/", async (req, res) => {
 	const userID = req.user.id;
@@ -10,6 +11,9 @@ router.get("/", async (req, res) => {
 		"SELECT * FROM tour WHERE user_id = ? or team_id = ?",
 		[userID, teamID]
 	);
+	tours.forEach(singleTour => {
+		singleTour.date = moment(singleTour.date).utc().format("YYYY-MM-DD");
+	});
 	res.render("tour/index.hbs", { tours });
 });
 
@@ -47,6 +51,35 @@ router.post("/team", hasTeam, async (req, res) => {
 		team_id: teamId,
 	};
 	await pool.query("INSERT INTO tour set ?", [newTour]);
+	res.redirect("/tour");
+});
+
+// DELETE AND EDIT OPTIONS
+
+router.get("/delete/:id", async (req, res) => {
+	const tourId = req.params.id;
+	await pool.query("DELETE FROM tour WHERE id = ?", [tourId]);
+	res.redirect("/tour");
+});
+
+router.get("/edit/:id", async (req, res) => {
+	const tourId = req.params.id;
+	const tours = await pool.query("SELECT * FROM tour WHERE id = ?", [tourId]);
+	const tour = tours[0];
+	tour.date = moment(tour.date).utc().format("YYYY-MM-DD");
+	res.render("tour/edit.hbs", { team: false, tour });
+});
+
+router.post("/edit/:id", async (req, res) => {
+	const tourId = req.params.id;
+	const { duration, distance, description, date } = req.body;
+	const newTour = {
+		duration,
+		distance,
+		description,
+		date,
+	};
+	await pool.query("UPDATE tour SET ? WHERE id = ?", [newTour, tourId]);
 	res.redirect("/tour");
 });
 
